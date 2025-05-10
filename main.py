819500af94f65676f714a7674ad7ee9e2b2d42f3
MammadAlinia <mohammadalinia82@gmail.com>
2025-05-10 14:35:45 +0330

    adding layer class and activation functions

--- main.py
+++ main.py
@@ -1,9 +1,43 @@
 ﻿import numpy as np
-inputs = [1, 2, 3, 2.5]
-weights = [[0.2, 0.8, - 0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
-bias = [2,3,0.5]
+import nnfs
+from nnfs.datasets import spiral_data
 
-output = np.dot(weights,inputs) + bias
-print(output)
+nnfs.init()
+
+
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
